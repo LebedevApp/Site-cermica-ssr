901e65ac616bafd86a79af605d8f7f61e596d122
Dmitryy <llle8evvv@yandex.ru>
2021-08-21 09:46:54 +0300

    add meta index

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,19 +7,19 @@ export default {
   target: "server",
 
   // Global page headers: https://go.nuxtjs.dev/config-head
-  // head: {
+  head: {
   //   titleTemplate: "%s - site-ceramica-ssr",
   //   title: "site-ceramica-ssr",
   //   htmlAttrs: {
   //     lang: "ru"
   //   },
-  //   meta: [
-  //     { charset: "utf-8" },
-  //     { name: "viewport", content: "width=device-width, initial-scale=1" },
-  //     { hid: "description", name: "description", content: "" }
-  //   ],
-  //   link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
-  // },
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      // { hid: "description", name: "description", content: "" }
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+  },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
